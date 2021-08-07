69d0be8394455c53cc41fe337503b315e46b2d78
SophonJumpapang <Sophonj@hatari.net>
2021-08-07 20:57:38 +0700

    newDeck

--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamons", newCard()}
-	cards = append(cards, "Six of Spades")
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
-}
-
-func newCard() string {
-	return "Five of Diamons"
+	cards := newDeck()
+	cards.print()
 }
